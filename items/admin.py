9f32e60d966c3515f4ade149774366b4284c9d98
samitnuk <samitnuk.ua@gmail.com>
2016-07-31 15:59:55 +0300

    register Category model in admin panel

--- items/admin.py
+++ items/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Item, ItemChange
+from .models import Item, ItemChange, Category
 
 admin.site.register(Item)
 admin.site.register(ItemChange)
+admin.site.register(Category)
