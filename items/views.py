d91ad494fba9da84b8aaa27582a8841fae2caaf7
samitnuk <samitnuk.ua@gmail.com>
2016-07-31 20:27:44 +0300

    add main view

--- items/views.py
+++ items/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+from .models import Item
+
 
 def main(request):
-
-    return render(request, 'items/main.html', {})
+    items = Item.objects.order_by('category')
+    return render(request, 'items/main.html', {'items': items})
