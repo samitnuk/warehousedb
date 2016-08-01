789f09be3a6d42fea9117ae01d3180bba1c31692
samitnuk <samitnuk.ua@gmail.com>
2016-08-01 23:21:55 +0300

    add categories to context

--- items/views.py
+++ items/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
-from .models import Item
+from .models import Item, Category
 
 
 def main(request):
     items = Item.objects.order_by('category')
-    return render(request, 'items/main.html', {'items': items})
+    categories = Category.objects.order_by('name')
+    return render(request, 'items/main.html', {'items': items,
+                                               'categories': categories})
