9ef3022ae57483e4444bad3b6f769358d2897cbb
samitnuk <samitnuk.ua@gmail.com>
2016-08-04 23:02:56 +0300

    add ability to work with media files

--- warehousedb/urls.py
+++ warehousedb/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from items import views
 
@@ -29,4 +31,4 @@ urlpatterns = [
     url(r'^logout/', views.logout, name='logout'),
 
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
