d94826910be59e36d7bc1312e9d0ff74dc5bcde6
samitnuk <samitnuk.ua@gmail.com>
2016-08-16 23:28:44 +0300

    create logic for user login

--- items/views.py
+++ items/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth import login as login_user
 from django.contrib.auth import logout as logout_user
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth.models import User
+
 from django.http import HttpResponse
 
 from .models import Item, ItemChange, Category
@@ -105,7 +107,33 @@ def item_details(request, pk):
 
 
 def login(request):
-    return HttpResponse('<h1>LOGIN page</h1>')
+    if request.method == 'POST':
+
+        errors = {}
+
+        username = request.POST.get("username", "").strip()
+        if not username:
+            errors["username"] = "Логін є обов'язковим"
+        else:
+            if not User.objects.filter(username=username).exists():
+                errors["username"] = "Такого користувача не існує"
+                return render(request, 'items/login.html', {'errors': errors})
+
+        password = request.POST.get("password", "").strip()
+        if not password:
+            errors["password"] = "Пароль є обов'язковим"
+        else:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login_user(request, user)
+                return redirect('main')
+            else:
+                errors["password"] = "Неправильний пароль"
+
+        if errors:
+            return render(request, 'items/login.html', {'errors': errors})
+
+    return render(request, 'items/login.html', {})
 
 
 @login_required(login_url='/login/')
