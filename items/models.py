4648df06bc845bfdad79a3958a9f7d66a4930b1e
samitnuk <samitnuk.ua@gmail.com>
2016-09-01 22:09:09 +0300

    clean up code

--- items/models.py
+++ items/models.py
@@ -17,41 +17,22 @@ class Item(models.Model):
         verbose_name = "Позиція на складі"
         verbose_name_plural = "Позиції на складі"
 
-    title = models.CharField(
-        max_length=256,
-        blank=False,
-        verbose_name="Найменування",
-    )
+    title = models.CharField(max_length=256, blank=False,
+                             verbose_name="Найменування")
 
-    part_number = models.CharField(
-        max_length=256,
-        blank=True,
-        verbose_name="Індекс",
-    )
+    part_number = models.CharField(max_length=256, blank=True,
+                                   verbose_name="Індекс")
 
-    part_number2 = models.CharField(
-        max_length=256,
-        blank=True,
-        verbose_name="Наш індекс",
-    )
+    part_number2 = models.CharField(max_length=256, blank=True,
+                                    verbose_name="Наш індекс")
 
-    picture = models.ImageField(
-        blank=True,
-        null=True,
-        verbose_name="Зображення",
-    )
+    picture = models.ImageField(blank=True, null=True,
+                                verbose_name="Зображення")
 
-    category = models.ForeignKey(
-        'Category',
-        blank=True,
-        null=True,
-        verbose_name="Категорія",
-    )
+    category = models.ForeignKey('Category', blank=True, null=True,
+                                 verbose_name="Категорія")
 
-    notes = models.TextField(
-        blank=True,
-        verbose_name="Примітка",
-    )
+    notes = models.TextField(blank=True, verbose_name="Примітка")
 
     @property
     def current_total(self):
@@ -77,14 +58,9 @@ class ItemChange(models.Model):
         verbose_name = "Зміна кількості"
         verbose_name_plural = "Зміни кількості"
 
-    additional_quantity = models.FloatField(
-        blank=False,
-    )
+    additional_quantity = models.FloatField(blank=False)
 
-    item = models.ForeignKey(
-        'Item',
-        related_name='quantity',
-    )
+    item = models.ForeignKey('Item', related_name='quantity')
 
     changed_at = models.DateField(
         blank=False,
@@ -92,10 +68,7 @@ class ItemChange(models.Model):
         verbose_name="Дата зміни",
     )
 
-    notes = models.TextField(
-        blank=True,
-        verbose_name="Примітка",
-    )
+    notes = models.TextField(blank=True, verbose_name="Примітка")
 
     def __str__(self):
         a_q = self.additional_quantity
@@ -117,61 +90,18 @@ class Category(models.Model):
         verbose_name = "Категорія"
         verbose_name_plural = "Категорії"
 
-    name = models.CharField(
-        max_length=256,
-        verbose_name="Назва",
-    )
+    name = models.CharField(max_length=256, verbose_name="Назва")
 
-    notes = models.TextField(
-        blank=True,
-        verbose_name="Примітка",
-    )
+    notes = models.TextField(blank=True, verbose_name="Примітка")
 
     def __str__(self):
         return self.name
 
 
-class Order(models.Model):
-    """Order Model
-
-       order = set of order_lines
-
-       Instance of Order model is
-       set of instances of OrderLine model
-
-    """
-
-    class Meta(object):
-        verbose_name = "Замовлення"
-        verbose_name_plural = "Замовлення"
-
-    customer = models.CharField(
-        max_length=256,
-        blank=False,
-        verbose_name="Замовник",
-    )
-
-    order_date = models.DateField(
-        blank=False,
-        default=date.today,
-        verbose_name="Дата зміни",
-    )
-
-    @property
-    def order_lines(self):
-        return self.order_line.all()
-
-    def __str__(self):
-        return '%s / %s' % (self.order_date, self.customer)
-
-
 class Product(models.Model):
     """Product Model
-       
-       product = set of components
 
-       Instance of Product model is
-       set of instances of Component model
+       Instance of Product Model is set of instances of Component Model
 
     """
 
@@ -179,28 +109,13 @@ class Product(models.Model):
         verbose_name = "Виріб"
         verbose_name_plural = "Вироби"
 
-    title = models.CharField(
-        max_length=256,
-        blank=False,
-        verbose_name="Найменування",
-    )
+    title = models.CharField(max_length=256, blank=False,
+                             verbose_name="Найменування")
 
-    part_number = models.CharField(
-        max_length=256,
-        blank=True,
-        verbose_name="Індекс",
-    )
+    part_number = models.CharField(max_length=256, blank=True,
+                                   verbose_name="Індекс")
 
-    created_at = models.DateField(
-        blank=False,
-        default=date.today,
-        verbose_name="Дата створення",
-    )
-
-    notes = models.TextField(
-        blank=True,
-        verbose_name="Примітка",
-    )
+    notes = models.TextField(blank=True, verbose_name="Примітка")
 
     @property
     def components(self):
@@ -212,34 +127,11 @@ class Product(models.Model):
         return self.title
 
 
-class OrderLine(models.Model):
-    """OrderLine Model
-    
-       Describes product and it quantity. 
-
-    """
-    class Meta(object):
-        verbose_name = "Поле замовлення"
-        verbose_name_plural = "Поля замовлення"
-
-    order = models.ForeignKey(Order, related_name='order_line')
-
-    product = models.ForeignKey(Product)
-
-    quantity = models.FloatField(blank=False)
-
-    def __str__(self):
-        return '%s - %s шт.' % (self.product, self.quantity)
-
 class Component(models.Model):
     """Component Model
 
        Describes component and quantity of this component
        in separate product.
-       
-       When instance of Component Model created,
-       will be created instace of ItemChange Model
-       using post_save signal.
 
     """
 
@@ -257,12 +149,49 @@ class Component(models.Model):
         return self.item
 
 
-@receiver(post_save, sender=Component)
+class Order(models.Model):
+    """Order Model
+
+       When instance of Order Model will be created,
+       will be created related instances of ItemChange Model
+       throught post_save signal.
+
+    """
+
+    class Meta(object):
+        verbose_name = "Замовлення"
+        verbose_name_plural = "Замовлення"
+
+    customer = models.CharField(max_length=256, blank=False,
+                                verbose_name="Замовник")
+
+    order_date = models.DateField(blank=False, default=date.today,
+                                  verbose_name="Дата зміни")
+
+    product = models.ForeignKey('Product')
+
+    quantity = models.FloatField(blank=False, verbose_name="Кількість")
+
+    def __str__(self):
+        return '%s / %s' % (self.order_date, self.customer)
+
+
+@receiver(post_save, sender=Order)
 def auto_create_item_change(instance, **kwargs):
 
-    order_line = OrderLine.objects.filter(product=instance.product).first()
+    order = instance
 
-    ItemChange.objects.create(
-        additional_quantity=-instance.quantity * order_line.quantity,
-        item=instance.item,
-    )
+    if order.products.part_number:
+        product_title = '%s - %s' % (order.product.part_number,
+                                     order.product.title)
+    else:
+        product_title = order.product.title
+
+    for component in order.product.components:
+        ItemChange.objects.create(
+            additional_quantity=order.quantity * component.quantity * -1,
+            item=component.item,
+            notes='%s / %s шт. / %s' % (product_title,
+                                        order.quantity,
+                                        order.customer)
+        )
