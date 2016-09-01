84cbbba951f26d4f2701672294c63c7d5e655612
samitnuk <samitnuk.ua@gmail.com>
2016-09-01 22:08:43 +0300

    move urls for items app to items.urls

--- warehousedb/urls.py
+++ warehousedb/urls.py
@@ -13,32 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from items import views
 
 urlpatterns = [
 
-    url(r'^$', views.main,
-        name='main'),
-    url(r'^category/(?P<pk>\d+)/', views.category,
-        name='category'),
-    url(r'^items_by_dates/', views.items_by_dates,
-        name='items_by_dates'),
-    url(r'^item_details/(?P<pk>\d+)/', views.item_details,
-        name='item_details'),
-    url(r'^item_change_details/(?P<pk>\d+)/', views.item_change_details,
-        name='item_change_details'),
-    url(r'^add_item_change/(?P<pk>\d+)/', views.add_item_change,
-        name='add_item_change'),
-
-    url(r'^login/', views.login,
-        name='login'),
-    url(r'^logout/', views.logout,
-        name='logout'),
-
     url(r'^admin/', admin.site.urls),
+
+    url(r'', include('items.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
