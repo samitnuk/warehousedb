a95a296814031ec532e7491fdad2e6c929a9a9f1
Basil Dubyk <samitnuk.ua@gmail.com>
2016-09-08 23:58:08 +0300

    update create_new_product view and template with forms

--- items/views/product.py
+++ items/views/product.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from ..models import Item, Product, Component
+from ..forms import AddProductForm
 
 
 @login_required(login_url='/login/')
@@ -10,43 +11,75 @@ def create_new_product(request):
     items = Item.objects.all()
     
     if request.method == 'POST':
+        form = AddProductForm(request.POST)
+        if form.is_valid():
+            product = Product.objects.create(
+                title=form.cleaned_data['title'],
+                part_number=form.cleaned_data['part_number'],
+                notes=form.cleaned_data['notes'],
+            )
 
-        errors = {}
+            for item in items:
+                quantity = form.cleaned_data['item_%s' % item.id]
+                print(quantity)
+                if quantity is not None and float(quantity) > 0:
+                    component = Component.objects.create(
+                        product=product,
+                        item=item,
+                        quantity=quantity,
+                    )
 
-        product_title = request.POST.get("product_title", "").strip()
-        if not product_title:
-            errors["product_title"] = "Найменування обов'язкове"
-            return render(request, 'items/create_new_product.html',
-                          {'items': items,
-                           'errors': errors})
+            return redirect('main')
 
-        product_part_number = request.POST.get("product_part_number", "") \
-                                          .strip()
-        product_notes = request.POST.get("product_notes", "")
+        else:
+            return render(request, 'items/create_new_product.html', 
+                {'items': items, 'form': form}
+            )
 
-        product = Product.objects.create(title=product_title,
-                                         part_number=product_part_number,
-                                         notes=product_notes)
+    # if GET or any other method
+    else:
+        form = AddProductForm(auto_id=False)
+        return render(request, 'items/create_new_product.html', 
+            {'items': items, 'form': form}
+        )
 
-        for item in items:
-            quantity_str = request.POST.get("%s" % item.id, "").strip()
-            if quantity_str:
-                try:
-                    quantity = float(quantity_str)
-                    component = Component(product=product,
-                                          item=item,
-                                          quantity=quantity)
-                    component.save()
-                except ValueError:
-                    errors[item.id] = "Вводити тільки числа"
+        # errors = {}
 
-        if errors:
-            render(request, 'items/create_new_product.html',
-                   {'items': items,
-                    'errors': errors})
+        # product_title = request.POST.get("product_title", "").strip()
+        # if not product_title:
+        #     errors["product_title"] = "Найменування обов'язкове"
 
-        return render(request, 'items/create_new_product.html',
-                   {'items': items})
+        # quantity = {}
+        # for item in items:
+        #     quantity_str = request.POST.get("%s" % item.id, "").strip()
+        #     if quantity_str:
+        #         try:
+        #             quantity["%s" % item.id] = float(quantity_str)
+        #         except ValueError:
+        #             errors[item.id] = "Вводити тільки числа"
 
-    return render(request, 'items/create_new_product.html',
-                  {'items': items})
+        # if errors:
+        #     return render(request, 'items/create_new_product.html',
+        #                   {'items': items,
+        #                    'errors': errors})
+        # else:
+        #     product_part_number = request.POST \
+        #                                  .get("product_part_number", "") \
+        #                                  .strip()
+        #     product_notes = request.POST.get("product_notes", "").strip()
+        #     product = Product.objects.create(title=product_title,
+        #                                      part_number=product_part_number,
+        #                                      notes=product_notes)
+        #     for item in items:
+        #         if quantity["%s" % item.id] > 0:
+        #             component = Component(product=product,
+        #                                   item=item,
+        #                                   quantity=quantity[item.id])
+        #             component.save()
+
+    # return render(request, 'items/create_new_product.html',
+    #               {'items': items})
+
+@login_required(login_url='/login/')
+def products(request):
+    pass
