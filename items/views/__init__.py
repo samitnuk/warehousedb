28e87db5cc15195674f9524f26262e852aba75f5
Basil Dubyk <samitnuk.ua@gmail.com>
2016-09-09 23:05:00 +0300

    change import

--- items/views/__init__.py
+++ items/views/__init__.py
@@ -1,11 +1,5 @@
-from .item import (main,
-                   category,
-                   items_by_dates,
-                   item_details,
-                   item_change_details,
-                   add_item_change)
+from .item import *
 
-from .product import create_new_product, products
+from .product import *
 
 from .login import login, logout
-
