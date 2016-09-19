81fa2fd84efc0cfb2f78e9d9002af6a7caca1a6e
Basil Dubyk <samitnuk.ua@gmail.com>
2016-09-19 21:36:07 +0300

    update tests

--- items/tests.py
+++ items/tests.py
@@ -2,21 +2,44 @@ from django.test import TestCase
 
 from .models import Item, Category
 
+
+def create_category(name, notes):
+    return Category.objects.create(
+        name=name,
+        notes=notes)
+
+def create_item(title, part_number, part_number2, picture, category, notes):
+    return Item.objects.create(
+        title=title,
+        part_number=part_number,
+        part_number2=part_number2,
+        picture=picture,
+        category=category,
+        notes=notes)
+
 class ItemTests(TestCase):
 
-    def test_item_creation(self):
+    def test_category_creation(self):
 
-        category = Category.objects.create(
+        category = create_category(
             name="Тестова категорія",
             notes="Тестова нотатка")
         self.assertIs(category.name, "Тестова категорія")
         self.assertIs(category.notes, "Тестова нотатка")
 
-        item = Item.objects.create(
+    def test_item_creation(self):
+
+        item = create_item(
             title="Деталь 1",
             part_number="00_123",
-            category=category)
+            part_number2="",
+            picture="",
+            category=create_category(
+                name="Тестова категорія",
+                notes="Тестова нотатка"),
+            notes="Тестова нотатка деталі")
         self.assertIs(item.title, "Деталь 1")
         self.assertIs(item.part_number, "00_123")
         self.assertIs(item.category.name, "Тестова категорія")
         self.assertIs(item.category.notes, "Тестова нотатка")
+        self.assertIs(item.picture, None)
