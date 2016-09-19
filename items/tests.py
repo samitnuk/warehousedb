dee4ccdbc4079014707854ffc3117ddad4df654d
Basil Dubyk <samitnuk.ua@gmail.com>
2016-09-19 21:24:11 +0300

    add first test

--- items/tests.py
+++ items/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Item, Category
+
+class ItemTests(TestCase):
+
+    def test_item_creation(self):
+
+        category = Category.objects.create(
+            name="Тестова категорія",
+            notes="Тестова нотатка")
+        self.assertIs(category.name, "Тестова категорія")
+        self.assertIs(category.notes, "Тестова нотатка")
+
+        item = Item.objects.create(
+            title="Деталь 1",
+            part_number="00_123",
+            category=category)
+        self.assertIs(item.title, "Деталь 1")
+        self.assertIs(item.part_number, "00_123")
+        self.assertIs(item.category.name, "Тестова категорія")
+        self.assertIs(item.category.notes, "Тестова нотатка")
