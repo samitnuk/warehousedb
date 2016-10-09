a435b0ec4f2b1d8cde0553ce5f75fffdddbc47e8
Basil Dubyk <samitnuk.ua@gmail.com>
2016-10-09 15:08:26 +0300

    add new tests

--- items/tests.py
+++ items/tests.py
@@ -3,6 +3,8 @@ from django.test import TestCase, Client
 
 from .models import Item, Category
 
+from .utils import *
+
 
 def create_user(username, password):
     return User.objects.create(
@@ -72,3 +74,46 @@ class ItemTests(TestCase):
         self.assertEqual(item.category.name, "Тестова категорія")
         self.assertEqual(item.category.notes, "Тестова нотатка")
         # self.assertEqual(item.picture, None)
+
+
+class ProductTests(TestCase):
+
+    fixtures = ['items/load_data.json']
+
+    conduit = Item.objects.filter(title="Кожух").first()
+    core = Item.objects.filter(title="Сердечник").first()
+
+    def test_std_cable_creation(self):
+
+        create_std_cable(
+            conduit_id=self.conduit.id,
+            core_id=self.core.id,
+            serie=4,
+            travel=3,
+            mounting="23",
+            is_steel_rods=True,
+            is_steel_sleeves=False,
+            is_plastic_sleeves=False,
+            length=1500)
+
+        cable = Product.objects.all().first()
+        self.assertEqual(cable.part_number, "100.M4323.01500")
+        self.assertEqual(len(cable.components), 12)
+
+        core = cable.components.filter(item__title="Сердечник").first()
+        conduit = cable.components.filter(item__title="Кожух").first()
+        self.assertEqual(core.quantity, 1500 - 224 + 15)
+        self.assertEqual(conduit.quantity, 1500 - 406 + 15)
+
+    def test_tza_cable_creation(self):
+
+        create_tza_cable(
+            conduit_id=self.conduit.id,
+            core_id=self.core.id,
+            is_steel_rods=True,
+            length=7500)
+
+        cable = Product.objects.all().first()
+
+        self.assertEqual(cable.part_number, "ТЗА-100.М4(110)20.07500-01")
+        self.assertEqual(len(cable.components), 13)
