d87b22c0de8cdef0147db6f120710b6a5a7469f9
Basil Dubyk <samitnuk.ua@gmail.com>
2016-10-24 23:25:43 +0300

    clean up code

--- items/views/login.py
+++ items/views/login.py
@@ -23,34 +23,6 @@ def login(request):
 
     return render(request, 'items/login.html', {'form': form})
 
-    # if request.method == 'POST':
-    #
-    #     errors = {}
-    #
-    #     username = request.POST.get("username", "").strip()
-    #     if not username:
-    #         errors["username"] = "Логін є обов'язковим"
-    #     else:
-    #         if not User.objects.filter(username=username).exists():
-    #             errors["username"] = "Такого користувача не існує"
-    #             return render(request, 'items/login.html', {'errors': errors})
-    #
-    #     password = request.POST.get("password", "").strip()
-    #     if not password:
-    #         errors["password"] = "Пароль є обов'язковим"
-    #     else:
-    #         user = authenticate(username=username, password=password)
-    #         if user is not None:
-    #             login_user(request, user)
-    #             return redirect('item_list')
-    #         else:
-    #             errors["password"] = "Неправильний пароль"
-    #
-    #     if errors:
-    #         return render(request, 'items/login.html', {'errors': errors})
-    #
-    # return render(request, 'items/login.html', {})
-
 
 @login_required(login_url='/login/')
 def logout(request):
