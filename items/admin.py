af6467b2ff72cc0f8777d1c61dbcddc334c0a329
Basil Dubyk <samitnuk.ua@gmail.com>
2016-12-29 21:41:42 +0200

    Upgrade Item view in Admin Panel

--- items/admin.py
+++ items/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 
 import items.models as models
 
-admin.site.register(models.Item)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'part_number', 'part_number2', 'category', 'notes']
+    ordering = ['title']
+    list_filter = ['category']
+    list_per_page = 50
+
+    def view_on_site(self, obj):
+        return reverse('item_detail', kwargs={'pk': obj.id})
+
+admin.site.register(models.Item, ItemAdmin)
 admin.site.register(models.ItemChange)
 admin.site.register(models.Category)
 admin.site.register(models.Order)
