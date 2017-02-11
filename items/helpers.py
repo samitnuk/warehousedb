ca8b588a8f96504cc4a4ab063451ed64ca524f5e
Basil Dubyk <samitnuk.ua@gmail.com>
2017-02-11 19:30:06 +0200

    Fix issues was made because of previous fixes (:

--- items/helpers.py
+++ items/helpers.py
@@ -26,28 +26,31 @@ def get_date_range(range_start, range_stop):
     return date_range
 
 
-def get_categories_list(
-        range_start,
-        range_stop,
-        object_model,
-        objectchange_model,
-        field_name
-):
+def get_objects_list(objects_, total_changes, field_name):
     """
-    Return list of lists -> [object, [changes of this object]]
+    Return list of of next lists: [object, [changes of this object]]
     """
-    total_changes = objectchange_model.objects.filter(
-        changed_at__gte=range_start,
-        changed_at__lte=range_stop)
+    objects_list = []
+    for object_ in objects_:
+        objects_list.append(
+            [object_, total_changes.filter(**{field_name: object_})])
+    return objects_list
+
+
+def get_categories_list(objects_, total_changes, field_name):
+    """
+    Return list of next lists: [category, objects_list]
+    """
     categories = Category.objects.all()
-    categories_list = {}
+    categories_list = dict()
     for category in categories:
-        objects_ = object_model.objects.filter(category=category)
+        category_objects = objects_.filter(category=category)
         if objects_:
-            categories_list[category.title] = []
-            for object_ in objects_:
-                categories_list[category.title].append(
-                    [object_, total_changes.filter(**{field_name: object_})])
+            categories_list[category.title] = get_objects_list(
+                objects_=category_objects,
+                total_changes=total_changes,
+                field_name=field_name
+            )
     return sorted(categories_list.items())
 
 
@@ -62,13 +65,22 @@ def get_context_for_list_by_dates(
         range_start = datetime.today() - timedelta(days=7)
     if range_stop is None:
         range_stop = datetime.today()
+
+    data = dict()
+    data['objects_'] = object_model.objects.all()
+    data['total_changes'] = objectchange_model.objects.filter(
+        changed_at__gte=range_start,
+        changed_at__lte=range_stop)
+    data['field_name'] = field_name
+
+    if field_name == 'item':
+        data_list = get_categories_list(**data)
+    else:
+        data_list = get_objects_list(**data)
+
     context = {
         'initial_range_start': datetime.strftime(range_start, "%Y-%m-%d"),
         'initial_range_stop': datetime.strftime(range_stop, "%Y-%m-%d"),
         'date_range': get_date_range(range_start, range_stop),
-        'categories_list': get_categories_list(range_start,
-                                               range_stop,
-                                               object_model,
-                                               objectchange_model,
-                                               field_name)}
+        'data_list': data_list}
     return context
