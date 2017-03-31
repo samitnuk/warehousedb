1acd490dc891b65b2ecd38fe40ec6984cf46bb4d
Basil Dubyk <samitnuk.ua@gmail.com>
2017-03-31 21:14:50 +0300

    Update settings to use memcached

--- warehousedb/settings.py
+++ warehousedb/settings.py
@@ -73,21 +73,27 @@ DATABASES = {
 }
 
 
+# Cache
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+
 # Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.'
+             'UserAttributeSimilarityValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.'
+             'MinimumLengthValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.'
+             'CommonPasswordValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.'
+             'NumericPasswordValidator', },
 ]
 
 
